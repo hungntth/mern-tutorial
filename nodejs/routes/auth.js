382a5e8d49ea00746aebb5d74bc9e43cfef04bc8
Hung Nguyen <1119thaihungnguyen@gmail.com>
2022-05-09 07:52:53 +0700

    Validate Email Register

--- nodejs/routes/auth.js
+++ nodejs/routes/auth.js
@@ -2,21 +2,27 @@ const express = require("express");
 const Account = require("../app/models/Account");
 const router = express.Router();
 const CryptoJS = require("crypto-js");
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
+const emailvalidator = require("email-validator");
 
 router.post("/register", async (req, res) => {
     try {
         const acc = await Account.findOne({ email: req.body.email });
         if (acc) {
-            res.status(401).json("Account already exists");
+            res.status(400).json("Account already exists");
             return;
         }
-        const newAccount = await new Account({
-            email: req.body.email,
-            password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
-        }).save();
-        const accessToken = jwt.sign({ id: newAccount._id }, process.env.SECRET_KEY, { expiresIn: "5d" })
-        res.json({ newAccount, token: accessToken });
+        if (emailvalidator.validate(req.body.email)) {
+            const newAccount = await new Account({
+                email: req.body.email,
+                password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString(),
+            }).save();
+            const accessToken = jwt.sign({ id: newAccount._id }, process.env.SECRET_KEY, { expiresIn: "5d" })
+            res.json({ newAccount, token: accessToken });
+        } else {
+            res.status(400).json('Invalid email format.');
+        }
+
     } catch (err) {
         res.send(err);
     }
